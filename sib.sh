866dd1956461ed35c6a76f2c10ebe18c72a88154
Felipe Paixão <fleap@fleap.dev>
2024-11-14 15:22:24 -0300

    Use "--commit HEAD" on step 4

--- sib.sh
+++ sib.sh
@@ -4,13 +4,17 @@ PATCH_DIR="./patches"
 SOURCE_DIR="./source"
 
 # Step 1: build the project on the current state, before patch.
-./source/build.sh
+# This is a script that builds the project. $CFLAGS and $CXXFLAGS are configured to use the clang plugin.
+echo "[STEP 1] Building project..."
+(cd $SOURCE_DIR && ./build.sh)
 
 # Step 2: use mpc to create the current LRDB
+echo "[STEP 2] Analyzing project..."
 mkdir ./lrdb
 mpc analyze --storage ./lrdb --commit=HEAD --variant=current --dump-only ${SOURCE_DIR}
 
 # Step 3: apply patches
+echo "[STEP 3] Applying patches..."
 for patch in "$PATCH_DIR"/*.patch; do
     # Check if there are no patch files
     if [ ! -f "$patch" ]; then
@@ -29,5 +33,7 @@ for patch in "$PATCH_DIR"/*.patch; do
     fi
 done
 
-# Step 4: use mpc to analyze the current source
-mpc analyze --storage ./lrdb --commit=HEAD --check-storage ${SOURCE_DIR}
+# Step 4: use mpc to analyze the current source, after patches
+echo "[STEP 4] Analyzing results..."
+mpc analyze --storage ./lrdb --commit HEAD --check-storage ${SOURCE_DIR}
+
